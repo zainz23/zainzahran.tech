67c43c80f876031d1bd9a08d7afeb8b2007221da
zzahra2 <zzahra2@uic.edu>
2019-08-30 12:32:18 -0500

    Updates

---
+++ precache-manifest.fc75e8abb11c21302ee4c231472b0159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bea5f7e551a7ed7e9934b6b71533c7",
+    "url": "zainzahran.tech/index.html"
+  },
+  {
+    "revision": "f641ee19a83d3b229112",
+    "url": "zainzahran.tech/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "79bc74d681f9b9320fa5",
+    "url": "zainzahran.tech/static/js/2.5bd39ee8.chunk.js"
+  },
+  {
+    "revision": "f641ee19a83d3b229112",
+    "url": "zainzahran.tech/static/js/main.d14cbd90.chunk.js"
+  },
+  {
+    "revision": "236f716220e36a7867d4",
+    "url": "zainzahran.tech/static/js/runtime~main.8135fea9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "zainzahran.tech/static/media/logo.5d5d9eef.svg"
+  }
+]);
