f44b28611b6cb40f1be3418ec18497e035891d86
zzahra2 <zzahra2@uic.edu>
2019-09-06 18:55:10 -0500

    Fixes to sizing and responsive design

--- zainzahran-tech/src/components/Home.js
+++ zainzahran-tech/src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
     render() {
         return(
             <div className='home'>
-            	<iframe src="assets/gh_web/index.html" width="100%" style={{minHeight: '83vh'}} />
+            	<iframe src="assets/gh_web/index.html" width="100%" style={{minHeight: '83vh'}} title="game" />
             </div>
         )
     }
